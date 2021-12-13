d19298391907d4670ec83a356ef0677e64c08c2c
KonstigSwe <swekonstig@gmail.com>
2021-12-13 09:31:40 +0100

    Add project files.

---
+++ ArduinoC_IotMusicThingy.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoC_IotMusicThingy.ino
+ Created:	12/13/2021 9:29:37 AM
+ Author:	nikla
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
