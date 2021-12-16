39fed549c9ee9f57cdcaff690b66e1bbc820764f
Attben <mrtobone@gmail.com>
2021-12-16 17:50:32 +0100

    Added rickroll intro

--- ArduinoC_IotMusicThingy.ino
+++ ArduinoC_IotMusicThingy.ino
@@ -8,6 +8,9 @@
 #include <Key.h>
 #include "pitches.h"
 
+const unsigned int SECONDS_PER_MINUTE = 60u;
+const unsigned int MILLISECONDS_PER_SECOND = 1000u;
+
 const int ROW_NUM = 4; //four rows
 const int COLUMN_NUM = 4; //four columns
 const int BUZZER_PIN = 13;
@@ -28,44 +31,101 @@ char holdKey;
 unsigned long t_hold;
 
 struct note {
-	int frequency;
-	int duration;
-	int delay; //tone() doesn't wait for previous note to finish, so we manually wait a while.
+	unsigned int frequency;
+	unsigned int duration;
+	unsigned int delay; //tone() doesn't wait for previous note to finish, so we manually wait a while.
 };
 
 //Ugly hardcoded melodies
-
 void flamingo() {
-	//16th note at 89 BPM: 169 ms
-	//bar 1
-	int NoteArray[]{ NOTE_GS4 ,NOTE_AS4 ,NOTE_C5,NOTE_DS5,NOTE_F5, NOTE_F5 ,NOTE_G5,NOTE_F5,NOTE_DS5,NOTE_C5,NOTE_F5,NOTE_DS5,NOTE_G4,NOTE_GS4,NOTE_AS4,NOTE_C5,NOTE_DS5,
-		NOTE_F5,NOTE_F5 ,NOTE_G5,NOTE_F5 ,NOTE_DS5,NOTE_D5,NOTE_AS4, NOTE_G4,NOTE_C5,NOTE_G4,NOTE_C5,NOTE_AS4,NOTE_D4,NOTE_DS4 ,NOTE_F4,NOTE_GS4,NOTE_DS5,NOTE_F4,NOTE_GS4,NOTE_DS5,NOTE_F4
-	,NOTE_DS5,NOTE_DS5,NOTE_F4, NOTE_DS5,NOTE_F4,NOTE_AS4,NOTE_F4,NOTE_G4,NOTE_AS4, NOTE_F4, NOTE_GS4, NOTE_DS5, NOTE_F4, NOTE_C5, NOTE_D5, NOTE_DS5};
-	int NoteTime[]{ 169,169,169,169,169,169 / 2,169 / 2,169,169,169,169,169 * 2,169,169,169,169,169,169,169 / 2,169 / 2,169,169,169 * 4, 169 * 2,169*1.5, 169,169,169,169 * 2,169 / 2,169 / 2,
-	169,169,169,169,169,169,169,169,169,169,169,169,169,169,169,169, 169, 169 , 169 , 169 , 169 , 169, 169*2};
+	const unsigned int BPM = 89u;
+	const unsigned int t4th = MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE / BPM; //quarter note length (ms)
+	const unsigned int t8th = t4th / 2u;
+	const unsigned int t16th = t8th / 2u;
+	const unsigned int t32nd = t16th / 2u;
 
-	int DelayTime[]{ 169,169,169,169 * 2,169 * 2,169 / 2,169 / 2,169 * 2,169 * 2,169 * 2,169 * 2,169 * 3,169,169,169,169,169 * 2,169 * 2,169 / 2,169 / 2,169 * 2,169 * 2,169 * 4
-		,169 * 3,169*2, 169,169,169,169 * 3,169 / 2,169 / 2,
-	169,169,169,169,169,169,169,169,169,169,169,169,169,169,169,169, 169, 169, 169, 169, 169*2, 169 * 2, 169 * 2};
+	note notes[]{
+		//Bar 1 / 5
+		{NOTE_GS4, t16th, t16th}, {NOTE_AS4, t16th, t16th}, {NOTE_C5, t16th, t16th}, {NOTE_DS5, t16th, t8th},
+		{NOTE_F5, t16th, t8th},	{NOTE_F5, t32nd, t32nd}, {NOTE_G5, t32nd, t32nd}, {NOTE_F5, t16th, t8th},
+		{NOTE_DS5, t16th, t8th}, {NOTE_C5, t16th, t8th}, {NOTE_F5, t16th, t8th},
+		//Bar 2 / 6
+		{NOTE_DS5, t8th, t16th*3}, {NOTE_G4, t16th, t16th}, {NOTE_GS4, t16th, t16th}, {NOTE_AS4, t16th, t16th},
+		{NOTE_C5, t16th, t16th}, {NOTE_DS5, t16th, t8th}, {NOTE_F5, t16th, t8th}, {NOTE_F5, t32nd, t32nd},
+		{NOTE_G5, t32nd, t32nd}, {NOTE_F5, t16th, t8th}, {NOTE_DS5, t16th, t8th},
+		//Bar 3 / 7
+		{NOTE_D5, t4th, t4th}, {NOTE_AS4, t8th, t16th*3}, {NOTE_G4, t16th*1.5, t8th}, {NOTE_C5, t16th, t16th},
+		{NOTE_G4, t16th, t16th}, {NOTE_C5, t16th, t16th}, {NOTE_AS4, t8th, t16th*3}, {NOTE_G4, t16th, t32nd}, {NOTE_FS4, t16th, t32nd},
+		//Bar 4
+		{NOTE_F4, t16th, t16th}, {NOTE_GS4, t16th, t16th}, {NOTE_DS5, t16th, t16th}, {NOTE_F4, t16th, t16th},
+		{NOTE_GS4, t16th, t16th}, {NOTE_DS5, t16th, t16th}, {NOTE_F4, t16th, t16th}, {NOTE_DS5, t16th, t16th},
+		{NOTE_DS5, t16th, t16th}, {NOTE_F4, t16th, t16th}, {NOTE_DS5, t16th, t16th}, {NOTE_F4, t16th, t16th},
+		{NOTE_DS5, t16th, t16th}, {NOTE_F4, t16th, t16th}, {NOTE_G4, t16th, t16th}, {NOTE_AS4, t16th, t16th},
+		//Bar 8
+		{NOTE_F4, t16th, t16th}, {NOTE_GS4, t16th, t16th}, {NOTE_DS5, t16th, t16th}, {NOTE_F4, t16th, t16th},
+		{NOTE_C5, t16th, t8th}, {NOTE_D5, t16th, t8th}, {NOTE_DS5, t8th, t8th}
+	};
+
+	//Bars 1-4
 	for (int i = 0; i < 47; i++)
 	{
-		tone(BUZZER_PIN, NoteArray[i], NoteTime[i]);
-		delay(DelayTime[i]);
+		tone(BUZZER_PIN, notes[i].frequency, notes[i].duration);
+		delay(notes[i].delay);
 	}
+	//Bars 5-7 (repetitions of 1-3)
 	for (int i = 0; i < 31; i++)
 	{
-		tone(BUZZER_PIN, NoteArray[i], NoteTime[i]);
-		delay(DelayTime[i]);
+		tone(BUZZER_PIN, notes[i].frequency, notes[i].duration);
+		delay(notes[i].delay);
 	}
+	//Bar 8
 	for (int i = 47; i < 54; ++i) {
-		tone(BUZZER_PIN, NoteArray[i], NoteTime[i]);
-		delay(DelayTime[i]);
+		tone(BUZZER_PIN, notes[i].frequency, notes[i].duration);
+		delay(notes[i].delay);
 	}
 }
 
 void rickroll() {
-	tone(BUZZER_PIN,500, 1000000);
-	delay(100);
+	const unsigned int BPM = 110;
+	const unsigned int t4th = MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE / BPM; //quarter note length (ms)
+	const unsigned int t8th = t4th / 2u;
+	const unsigned int t16th = t8th / 2u;
+	//const unsigned int t32nd = t16th / 2u;
+
+	note intro[]{
+		{NOTE_CS5, t4th * 1.5, t4th * 1.5}, {NOTE_DS5, t4th * 1.5, t4th * 1.5}, {NOTE_GS4, t4th, t4th},
+		{NOTE_DS5, t4th * 1.5, t4th * 1.5}, {NOTE_F5, t4th * 1.5, t4th * 1.5},
+		{NOTE_GS5, t16th, t16th}, {NOTE_FS5, t16th, t16th}, {NOTE_F5, t8th, t8th},
+		{NOTE_CS5, t4th * 1.5, t4th * 1.5}, {NOTE_DS5, t4th * 1.5, t4th * 1.5}, {NOTE_GS4, t4th * 2, t4th * 5},
+	};
+	/*note verse[]{
+		{NOTE_AS4, t8th, t8th}, {NOTE_C5, t8th, t8th}, {NOTE_CS5, t8th, t8th}, {NOTE_CS5, t8th, t8th},
+		{NOTE_DS5, t8th, t8th}, {NOTE_C5, t8th * 1.5, t8th * 1.5}, {NOTE_AS4, t16th, t16th}, {NOTE_GS4, t4th, t4th * 4 + t8th},
+
+
+	};
+	note chorus[]{
+		{}
+	};*/
+
+	for (note n : intro) {
+		tone(BUZZER_PIN, n.frequency, n.duration);
+		delay(n.delay);
+	}
+	//Not a mistake; play it twice.
+	for (note n : intro) {
+		tone(BUZZER_PIN, n.frequency, n.duration);
+		delay(n.delay);
+	}
+	/*delay(t4th);
+	for (note n : verse) {
+		tone(BUZZER_PIN, n.frequency, n.duration);
+		delay(n.delay);
+	}
+	for (note n : chorus) {
+		tone(BUZZER_PIN, n.frequency, n.duration);
+		delay(n.delay);
+	}*/
 }
 
 void setup() {
@@ -122,10 +182,10 @@ void loop() {
 			break;
 		case '0':
 			flamingo();
-			break;
+			return;
 		case '#':
 			rickroll();
-			break;
+			return;
 		}
 
 		switch (keypad.getState()) {
